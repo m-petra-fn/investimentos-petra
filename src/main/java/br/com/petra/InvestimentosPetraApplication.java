a7d4aa99e0efb1a7d62202f2a3992be698187ffb
Marcela Petra Ferraz de Novaes <novaes.marcela@outlook.com>
2026-03-08 02:53:12 -0300

    first commit

---
+++ src/main/java/br/com/petra/InvestimentosPetraApplication.java
@@ -0,0 +1,13 @@
+package br.com.petra;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InvestimentosPetraApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InvestimentosPetraApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/com/petra/InvestimentosPetraApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.petra;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InvestimentosPetraApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
